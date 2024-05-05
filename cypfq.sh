9f0a028b6019038682851ad74651c14ddb0f9d9e
KevinFrax <kevin.vm1511@gmail.com>
2024-05-05 05:52:57 +0200

    Cloning Fix

--- cypfq.sh
+++ cypfq.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-echo "Sozosha Gay"
+echo "Sozosha"
 
 echo "Cloned OSS kernel"
-git clone --depth=1 https://github.com/cypfq-development/android_kernel_motorola_cypfq kernel/motorola/sm6375
+git clone --depth=1 https://github.com/cypfq-development/android_kernel_motorola_cypfq ../kernel/motorola/sm6375
 echo "Done"
 
 echo "Cloned OSS vendor"
-git clone https://github.com/cypfq-development/proprietary_vendor_motorola_sm6375-common vendor/motorola/sm6375-common
-git clone https://github.com/cypfq-development/proprietary_vendor_motorola_cypfq vendor/motorola/cypfq
+git clone https://github.com/cypfq-development/proprietary_vendor_motorola_sm6375-common ../vendor/motorola/sm6375-common
+git clone https://github.com/cypfq-development/proprietary_vendor_motorola_cypfq ../vendor/motorola/cypfq
 echo "Done"
 
 echo "Cloned Device tree"
-git clone https://github.com/cypfq-development/android_device_motorola_sm6375-common device/motorola/sm6375-common
-git clone https://github.com/cypfq-development/android_device_motorola_cypfq device/motorola/cypfq
+git clone https://github.com/cypfq-development/android_device_motorola_sm6375-common ../device/motorola/sm6375-common
+git clone https://github.com/cypfq-development/android_device_motorola_cypfq ../device/motorola/cypfq
 echo "Done"
 
 echo "Clone dependencies "
-git clone --depth=1 https://github.com/Sozosha-Dev/packages_apps_MotoActions -b fourteen-qpr2 packages/apps/MotoActions
-git clone https://github.com/LineageOS/android_system_qcom system/qcom
-git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
-git clone https://github.com/kenway214/vendor_bcr.git vendor/bcr
+git clone --depth=1 https://github.com/Sozosha-Dev/packages_apps_MotoActions -b fourteen-qpr2 ../packages/apps/MotoActions
+git clone https://github.com/LineageOS/android_system_qcom ../system/qcom
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX ../packages/apps/ViPER4AndroidFX
+git clone https://github.com/kenway214/vendor_bcr.git ../vendor/bcr
 echo "Done"
--- hanoip.sh
+++ hanoip.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
+echo "Kevin"
 
-echo "Cloned OSS kernel"
-git clone --depth=1 https://github.com/Sozosha-Dev/kernel_motorola_sm6150 kernel/motorola/hanoip
+echo "Cloning kernel"
+git clone --depth=1 https://github.com/Sozosha-Dev/kernel_motorola_sm6150 ../kernel/motorola/hanoip
 echo "Done"
 
-echo "Cloned OSS vendor"
-git clone https://github.com/Hanoip-Development/proprietary_vendor_motorola_hanoip vendor/motorola/hanoip
+echo "Cloning OSS vendor"
+git clone https://github.com/Hanoip-Development/proprietary_vendor_motorola_hanoip ../vendor/motorola/hanoip
 echo "Done"
 
-echo "Cloned Device tree"
-git clone https://github.com/Hanoip-Development/android_device_motorola_hanoip device/motorola/hanoip
-echo "Done" 
+echo "Cloning Device tree"
+git clone https://github.com/Hanoip-Development/android_device_motorola_hanoip ../device/motorola/hanoip
+echo "Done"
 
-echo "Clone dependencies "
-git clone --depth=1 https://github.com/Sozosha-Dev/packages_apps_MotoActions -b fourteen-qpr2 packages/apps/MotoActions
-
-git clone https://github.com/LineageOS/android_system_qcom system/qcom
-
-git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
-
-git clone https://github.com/kenway214/vendor_bcr.git vendor/bcr
+echo "Cloning dependencies"
+git clone --depth=1 https://github.com/Sozosha-Dev/packages_apps_MotoActions -b fourteen-qpr2 ../packages/apps/MotoActions
+git clone https://github.com/LineageOS/android_system_qcom ../system/qcom
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX ../packages/apps/ViPER4AndroidFX
+git clone https://github.com/kenway214/vendor_bcr.git ../vendor/bcr
 echo "Done"
